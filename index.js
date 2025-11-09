1e6c941b3a567ba059f15e120d5c712da87a5f53
HomayraHeme <heme5674@gmail.com>
2025-11-09 18:45:09 +0600

    delete books

--- index.js
+++ index.js
@@ -94,6 +94,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/books/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await booksCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
     } finally {
